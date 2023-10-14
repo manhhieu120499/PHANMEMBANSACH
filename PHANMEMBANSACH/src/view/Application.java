01ba68ea4fdb0711fddea0171f005e6e1fbb9f03
manhhieu120499 <93460932+manhhieu120499@users.noreply.github.com>
2023-10-14 22:06:33 +0700

    init project

---
+++ PHANMEMBANSACH/src/view/Application.java
@@ -0,0 +1,10 @@
+package view;
+
+import ui.GUI_TrangChu;
+
+public class Application {
+    public static void main(String[] args) {
+		GUI_TrangChu frame = new GUI_TrangChu();
+		frame.setVisible(true);
+	}
+}
