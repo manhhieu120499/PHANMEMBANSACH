3643f2f11e493a581977d6d4bf4b2a0a294e6bdd
Thuy Kieu <thuykieu130303@gmail.com>
2023-10-24 00:05:47 +0700

    giao dien sach

--- PHANMEMBANSACH/src/ui/GUISanPham.java
+++ PHANMEMBANSACH/src/ui/GUISanPham.java
@@ -4,61 +4,71 @@ import java.awt.Color;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import java.awt.FlowLayout;
 import javax.swing.JTabbedPane;
-import javax.swing.border.LineBorder;
+import javax.swing.JTable;
 
 import CustomUI.MyButton;
 import CustomUI.MyCombobox;
+import CustomUI.MyTable;
 
 import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.Font;
+import java.awt.SystemColor;
 
 public class GUISanPham extends JPanel {
 	private JTabbedPane tabbedPane;
-	private JLabel lblMaSanPham;
-	private JLabel lblTenSanPham;
-	private JLabel lblNhaCungCap;
+	private JLabel lblMaSP;
+	private JLabel lblTenSP;
+	private JLabel lblNCC;
 	private JLabel lblKe;
 	private JLabel lblTheLoai;
 	private JTextField txtTheLoai;
-	private JTextField txtMaSanPham;
-	private JTextField txtTenSanPham;
+	private JTextField txtMaSP;
+	private JTextField txtTenSP;
 	private JTextField txtKe;
 	private JLabel lblSoLuong;
 	private JLabel lblGiaNhap;
 	private JLabel lblThue;
 	private JLabel lblLoiNhuan;
 	private JLabel lblTacGia;
-	private JLabel lblNhaXuatBan;
-	private JLabel lblNamXuatBan;
+	private JLabel lblNhaXB;
+	private JLabel lblNamXB;
 	private JTextField txtSoLuong;
 	private JTextField txtGiaNhap;
 	private JTextField txtThue;
 	private JTextField txtLoiNhuan;
 	private JTextField txtTacGia;
-	private JTextField txtNhaXuatBan;
-	private JTextField txtxNamXuatBan;
+	private JTextField txtNhaXB;
+	private JTextField txtNamXB;
 	private JPanel pnlThongTin;
 	private JLabel lblPhanTramThue;
 	private JLabel lblPhanTramLN;
 	private JButton btnHinhAnh;
-	private JPanel panel;
+	private JPanel pnlTimKiem;
 	private JLabel lblTimKiem;
-	private JTextField textField;
+	private JTextField txtTimKiem;
 	private JButton btnTaoMa;
+	private JTextField txtLocTheLoai;
+	private JTextField txtLocNamXB;
+	private JTextField txtLocTacGia;
+	private DefaultTableModel model;
+	private JTable table;
 
 	public GUISanPham() {
 		this.setBackground(new Color(255, 255, 255));
 		this.setBounds(250, 0, 1250, 800);
-//		this.setLayout(new BorderLayout(0, 0));
 		this.setLayout(null);
-
-//		JButton btnNewButton = new JButton("Đây là sản phẩm");
-//		btnNewButton.setBounds(0, 513, 1034, -513);
-//		this.add(btnNewButton, BorderLayout.CENTER);
 
 		// Tạo tab
 		tabbedPane = new JTabbedPane();
@@ -71,168 +81,244 @@ public class GUISanPham extends JPanel {
 		pnlSach.setLayout(null);
 
 		pnlThongTin = new JPanel();
-		pnlThongTin.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		pnlThongTin.setBounds(10, 10, 995, 231);
+		pnlThongTin.setBounds(20, 10, 965, 231);
+		pnlThongTin.setBorder(new TitledBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null),
+				"Thông Tin Sản Phẩm", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		pnlSach.add(pnlThongTin);
 		pnlThongTin.setLayout(null);
 
-		lblMaSanPham = new JLabel("Mã sản phẩm");
-		lblMaSanPham.setBounds(25, 23, 90, 13);
-		pnlThongTin.add(lblMaSanPham);
+		lblMaSP = new JLabel("Mã sản phẩm");
+		lblMaSP.setBounds(25, 33, 90, 13);
+		pnlThongTin.add(lblMaSP);
 
-		lblTenSanPham = new JLabel("Tên sản phẩm");
-		lblTenSanPham.setBounds(25, 58, 90, 13);
-		pnlThongTin.add(lblTenSanPham);
+		lblTenSP = new JLabel("Tên sản phẩm");
+		lblTenSP.setBounds(25, 68, 90, 13);
+		pnlThongTin.add(lblTenSP);
 
-		lblNhaCungCap = new JLabel("Nhà cung cấp");
-		lblNhaCungCap.setBounds(25, 96, 90, 13);
-		pnlThongTin.add(lblNhaCungCap);
+		lblNCC = new JLabel("Nhà cung cấp");
+		lblNCC.setBounds(25, 106, 90, 13);
+		pnlThongTin.add(lblNCC);
 
 		lblTheLoai = new JLabel("Thể loại");
-		lblTheLoai.setBounds(25, 132, 90, 13);
+		lblTheLoai.setBounds(25, 142, 90, 13);
 		pnlThongTin.add(lblTheLoai);
 
 		lblKe = new JLabel("Kệ");
-		lblKe.setBounds(25, 169, 90, 13);
+		lblKe.setBounds(25, 179, 90, 13);
 		pnlThongTin.add(lblKe);
 
 		btnTaoMa = new MyButton("Tạo mã");
-		btnTaoMa.setBounds(357, 19, 96, 21);
+		btnTaoMa.setBounds(357, 29, 96, 21);
 		pnlThongTin.add(btnTaoMa);
 
-		txtMaSanPham = new JTextField();
-		txtMaSanPham.setEnabled(false);
-		txtMaSanPham.setColumns(10);
-		txtMaSanPham.setBounds(116, 20, 215, 19);
-		pnlThongTin.add(txtMaSanPham);
+		txtMaSP = new JTextField();
+		txtMaSP.setEnabled(false);
+		txtMaSP.setColumns(10);
+		txtMaSP.setBounds(116, 30, 215, 19);
+		pnlThongTin.add(txtMaSP);
 
-		txtTenSanPham = new JTextField();
-		txtTenSanPham.setColumns(10);
-		txtTenSanPham.setBounds(116, 56, 337, 19);
-		pnlThongTin.add(txtTenSanPham);
+		txtTenSP = new JTextField();
+		txtTenSP.setColumns(10);
+		txtTenSP.setBounds(116, 66, 337, 19);
+		pnlThongTin.add(txtTenSP);
 
 		JComboBox cboNhaCungCap = new MyCombobox();
-		cboNhaCungCap.setBounds(116, 92, 337, 21);
+		cboNhaCungCap.setBounds(116, 102, 337, 21);
 		pnlThongTin.add(cboNhaCungCap);
 
 		txtTheLoai = new JTextField();
-		txtTheLoai.setBounds(116, 130, 337, 19);
+		txtTheLoai.setBounds(116, 140, 337, 19);
 		pnlThongTin.add(txtTheLoai);
 		txtTheLoai.setColumns(10);
 
 		txtKe = new JTextField();
-		txtKe.setBounds(116, 166, 337, 19);
+		txtKe.setBounds(116, 176, 337, 19);
 		pnlThongTin.add(txtKe);
 		txtKe.setColumns(10);
 
 		lblSoLuong = new JLabel("Số lượng");
-		lblSoLuong.setBounds(528, 23, 90, 13);
+		lblSoLuong.setBounds(493, 33, 90, 13);
 		pnlThongTin.add(lblSoLuong);
 
 		lblGiaNhap = new JLabel("Giá nhập");
-		lblGiaNhap.setBounds(528, 58, 90, 13);
+		lblGiaNhap.setBounds(493, 68, 90, 13);
 		pnlThongTin.add(lblGiaNhap);
 
 		lblThue = new JLabel("Thuế");
-		lblThue.setBounds(795, 23, 45, 13);
+		lblThue.setBounds(760, 33, 45, 13);
 		pnlThongTin.add(lblThue);
 
 		lblLoiNhuan = new JLabel("Lợi nhuận ");
-		lblLoiNhuan.setBounds(775, 58, 68, 13);
+		lblLoiNhuan.setBounds(740, 68, 68, 13);
 		pnlThongTin.add(lblLoiNhuan);
 
 		lblTacGia = new JLabel("Tác giả");
-		lblTacGia.setBounds(528, 96, 90, 13);
+		lblTacGia.setBounds(493, 106, 90, 13);
 		pnlThongTin.add(lblTacGia);
 
-		lblNhaXuatBan = new JLabel("Nhà xuất bản");
-		lblNhaXuatBan.setBounds(528, 132, 90, 13);
-		pnlThongTin.add(lblNhaXuatBan);
+		lblNhaXB = new JLabel("Nhà xuất bản");
+		lblNhaXB.setBounds(493, 142, 90, 13);
+		pnlThongTin.add(lblNhaXB);
 
-		lblNamXuatBan = new JLabel("Năm xuất bản");
-		lblNamXuatBan.setBounds(528, 169, 90, 13);
-		pnlThongTin.add(lblNamXuatBan);
+		lblNamXB = new JLabel("Năm xuất bản");
+		lblNamXB.setBounds(493, 179, 90, 13);
+		pnlThongTin.add(lblNamXB);
 
 		txtSoLuong = new JTextField();
-		txtSoLuong.setBounds(618, 20, 120, 19);
+		txtSoLuong.setBounds(583, 30, 120, 19);
 		pnlThongTin.add(txtSoLuong);
 		txtSoLuong.setColumns(10);
 
 		txtGiaNhap = new JTextField();
-		txtGiaNhap.setBounds(618, 55, 120, 19);
+		txtGiaNhap.setBounds(583, 65, 120, 19);
 		pnlThongTin.add(txtGiaNhap);
 		txtGiaNhap.setColumns(10);
 
 		txtThue = new JTextField();
-		txtThue.setBounds(834, 20, 120, 19);
+		txtThue.setBounds(799, 30, 120, 19);
 		pnlThongTin.add(txtThue);
 		txtThue.setColumns(10);
 
 		txtLoiNhuan = new JTextField();
-		txtLoiNhuan.setBounds(834, 55, 120, 19);
+		txtLoiNhuan.setBounds(799, 65, 120, 19);
 		pnlThongTin.add(txtLoiNhuan);
 		txtLoiNhuan.setColumns(10);
 
 		txtTacGia = new JTextField();
-		txtTacGia.setBounds(618, 93, 336, 19);
+		txtTacGia.setBounds(583, 103, 336, 19);
 		pnlThongTin.add(txtTacGia);
 		txtTacGia.setColumns(10);
 
-		txtNhaXuatBan = new JTextField();
-		txtNhaXuatBan.setBounds(618, 129, 337, 19);
-		pnlThongTin.add(txtNhaXuatBan);
-		txtNhaXuatBan.setColumns(10);
+		txtNhaXB = new JTextField();
+		txtNhaXB.setBounds(583, 139, 337, 19);
+		pnlThongTin.add(txtNhaXB);
+		txtNhaXB.setColumns(10);
 
-		txtxNamXuatBan = new JTextField();
-		txtxNamXuatBan.setBounds(618, 166, 337, 19);
-		pnlThongTin.add(txtxNamXuatBan);
-		txtxNamXuatBan.setColumns(10);
+		txtNamXB = new JTextField();
+		txtNamXB.setBounds(583, 176, 337, 19);
+		pnlThongTin.add(txtNamXB);
+		txtNamXB.setColumns(10);
 
 		lblPhanTramThue = new JLabel("%");
-		lblPhanTramThue.setBounds(965, 23, 30, 13);
+		lblPhanTramThue.setBounds(930, 33, 30, 13);
 		pnlThongTin.add(lblPhanTramThue);
 
 		lblPhanTramLN = new JLabel("%");
-		lblPhanTramLN.setBounds(965, 58, 30, 13);
+		lblPhanTramLN.setBounds(930, 68, 30, 13);
 		pnlThongTin.add(lblPhanTramLN);
 
-		JPanel pnHinhAnh = new JPanel();
-		pnHinhAnh.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		pnHinhAnh.setForeground(new Color(255, 255, 255));
-		pnHinhAnh.setBounds(1040, 10, 150, 200);
-		pnlSach.add(pnHinhAnh);
-		pnHinhAnh.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel pnlHinhAnh = new JPanel();
+		pnlHinhAnh.setBounds(1006, 10, 197, 200);
+		pnlHinhAnh.setBorder(new LineBorder(new Color(0, 0, 0)));
+		pnlHinhAnh.setForeground(new Color(255, 255, 255));
+		pnlSach.add(pnlHinhAnh);
+		pnlHinhAnh.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 
-		// Tab sách
-		JPanel pnVPP = new JPanel();
-		pnVPP.setBounds(0, 0, 1230, 800);
-		pnVPP.setLayout(null);
+		btnHinhAnh = new MyButton("Chọn ảnh ");
+		btnHinhAnh.setBounds(1067, 220, 100, 21);
+		btnHinhAnh.setForeground(new Color(0, 0, 0));
+		pnlSach.add(btnHinhAnh);
+
+		pnlTimKiem = new JPanel();
+		pnlTimKiem.setBounds(20, 262, 910, 211);
+		pnlTimKiem.setBorder(new TitledBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null), "T\u00ECm Ki\u1EBFm", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		pnlSach.add(pnlTimKiem);
+		pnlTimKiem.setLayout(null);
+
+		lblTimKiem = new JLabel("Nhập mã hoặc tên sản phẩm cần tìm");
+		lblTimKiem.setBounds(25, 25, 201, 13);
+		pnlTimKiem.add(lblTimKiem);
+
+		txtTimKiem = new JTextField();
+		txtTimKiem.setForeground(Color.BLACK);
+		txtTimKiem.setBounds(234, 22, 651, 19);
+		pnlTimKiem.add(txtTimKiem);
+		txtTimKiem.setColumns(10);
+
+		JPanel pnlLoc = new JPanel();
+		pnlLoc.setBorder(new TitledBorder(new LineBorder(new Color(171, 173, 179)), "Lọc Sản Phẩm",
+				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		pnlLoc.setBackground(new Color(255, 255, 255));
+		pnlLoc.setBounds(25, 61, 863, 129);
+		pnlTimKiem.add(pnlLoc);
+		pnlLoc.setLayout(null);
+
+		JLabel lblLocNCC = new JLabel("Nhà cung cấp");
+		lblLocNCC.setBounds(25, 29, 90, 13);
+		pnlLoc.add(lblLocNCC);
+
+		JLabel lblLocTheLoai = new JLabel("Thể loại");
+		lblLocTheLoai.setBounds(468, 29, 70, 13);
+		pnlLoc.add(lblLocTheLoai);
+
+		JLabel lblLocNamXB = new JLabel("Năm xuất bản");
+		lblLocNamXB.setBounds(25, 72, 90, 13);
+		pnlLoc.add(lblLocNamXB);
+
+		JLabel lblLocTacGia = new JLabel("Tác giả");
+		lblLocTacGia.setBounds(468, 69, 70, 13);
+		pnlLoc.add(lblLocTacGia);
+
+		txtLocTheLoai = new JTextField();
+		txtLocTheLoai.setBounds(534, 26, 300, 19);
+		pnlLoc.add(txtLocTheLoai);
+		txtLocTheLoai.setColumns(10);
+
+		txtLocNamXB = new JTextField();
+		txtLocNamXB.setBounds(120, 69, 300, 19);
+		pnlLoc.add(txtLocNamXB);
+		txtLocNamXB.setColumns(10);
+
+		txtLocTacGia = new JTextField();
+		txtLocTacGia.setBounds(534, 66, 300, 19);
+		pnlLoc.add(txtLocTacGia);
+		txtLocTacGia.setColumns(10);
+
+		JComboBox cboLocNCC = new MyCombobox();
+		cboLocNCC.setBounds(120, 25, 300, 21);
+		pnlLoc.add(cboLocNCC);
 
 		tabbedPane.addTab("Sách", pnlSach);
-		
-		btnHinhAnh = new MyButton("Chọn ảnh ");
-		btnHinhAnh.setForeground(new Color(0, 0, 0));
-		btnHinhAnh.setBounds(1067, 220, 100, 21);
-		pnlSach.add(btnHinhAnh);
-		
-		panel = new JPanel();
-		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel.setToolTipText("");
-		panel.setBounds(10, 275, 995, 237);
-		pnlSach.add(panel);
-		panel.setLayout(null);
-		
-		lblTimKiem = new JLabel("Tìm kiếm ");
-		lblTimKiem.setBounds(25, 25, 60, 13);
-		panel.add(lblTimKiem);
-		
-		textField = new JTextField();
-		textField.setToolTipText("");
-		textField.setBounds(116, 22, 838, 19);
-		panel.add(textField);
-		textField.setColumns(10);
-		tabbedPane.addTab("Văn phòng phẩm", pnVPP);
-		add(tabbedPane);
 
+		JPanel pnlChucNang = new JPanel();
+		pnlChucNang.setBorder(new TitledBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null),
+				"Chức Năng", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		pnlChucNang.setBounds(945, 262, 258, 211);
+		pnlSach.add(pnlChucNang);
+		pnlChucNang.setLayout(null);
+
+		JButton btnThemSP = new MyButton("THÊM SẢN PHẨM");
+		btnThemSP.setBounds(60, 23, 150, 35);
+		pnlChucNang.add(btnThemSP);
+
+		JButton btnCapNhat = new MyButton("CẬP NHẬT ");
+		btnCapNhat.setBounds(60, 78, 150, 35);
+		pnlChucNang.add(btnCapNhat);
+
+		JButton btnXoaTrang = new MyButton("XÓA TRẮNG");
+		btnXoaTrang.setBounds(10, 136, 100, 35);
+		pnlChucNang.add(btnXoaTrang);
+
+		JButton btnTaiLai = new MyButton("TẢI LẠI");
+		btnTaiLai.setBounds(148, 136, 100, 35);
+		pnlChucNang.add(btnTaiLai);
+		
+		String[] cols = { "STT", "Mã SP", "Tên sản phẩm", "Nhà cung cấp", "Tác giả", "Thể loại", "Nhà xuất bản", "Năm xuất bản",
+				"Kệ", "Số lượng", "Giá nhập"};
+		model = new DefaultTableModel(cols, 0);
+		table = new MyTable(model);
+		JScrollPane scr = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scr.setLocation(20, 489);
+		scr.setSize(1183, 239);
+		pnlSach.add(scr);
+		
+		// Tab văn phòng phẩm
+		JPanel pnlVPP = new JPanel();
+		pnlVPP.setBounds(0, 0, 1230, 800);
+		pnlVPP.setLayout(null);
+
+		tabbedPane.addTab("Văn phòng phẩm", pnlVPP);
+		add(tabbedPane);
 	}
 }
